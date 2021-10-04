5b0096ac6dd3156b5f48d52f5a7bddefd7d6a5f9
MrT <Mrtlearns@gmail.com>
2021-10-04 19:08:41 +0200

    Create process_files.sh

---
+++ ux_transports/process_files.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# NOTE : Quote it else use array to avoid problems #
+cd ~/abaptransports
+
+FILES=R*
+for f in $FILES
+do
+  echo "Processing $f file..."
+  # take action on each file. $f store current file name
+  docker cp $f a4h:/usr/sap/trans/data
+  docker exec -it a4h bash -c "chmod 777 /usr/sap/trans/data/$f"
+
+done
+
+FILES=K*
+for f in $FILES
+do
+  echo "Processing $f file..."
+  # take action on each file. $f store current file name
+  docker cp $f a4h:/usr/sap/trans/cofiles
+  docker exec -it a4h bash -c "chmod 777 /usr/sap/trans/cofiles/$f"
+  tfile=${f:8}${f:0:7}  #construct transport file pattern
+  docker exec -it a4h su - a4hadm -c "tp addtobuffer $tfile A4H pf=/usr/sap/trans/bin/TP_DOMAIN_A4H.PFL u168"
+done
