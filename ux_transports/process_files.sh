2c8c987adb0fef895afa6e41e7d949ddfbe7f630
MrT <Mrtlearns@gmail.com>
2021-10-05 07:34:33 +0200

    Update process_files.sh

--- ux_transports/process_files.sh
+++ ux_transports/process_files.sh
@@ -2,24 +2,24 @@
 # NOTE : Quote it else use array to avoid problems #
 cd ~/abaptransports
 
-FILES=R*
+FILES=R*   #data files
 for f in $FILES
 do
   echo "Processing $f file..."
   # take action on each file. $f store current file name
   docker cp $f a4h:/usr/sap/trans/data
-  docker exec -it a4h bash -c "chmod 777 /usr/sap/trans/data/$f"
+  docker exec -it a4h chmod -R 777 /usr/sap/trans/data
 
 done
-  docker exec -it a4h chmod -R 777 /usr/sap/trans/
   
-FILES=K*
+  
+FILES=K*  #control files
 for f in $FILES
 do
   echo "Processing $f file..."
   # take action on each file. $f store current file name
   docker cp $f a4h:/usr/sap/trans/cofiles
-  docker exec -it a4h bash -c "chmod 777 /usr/sap/trans/cofiles/$f"
+  docker exec -it a4h chmod -R 777 /usr/sap/trans/cofiles
   tfile=${f:8}${f:0:7}  #construct transport file pattern
   docker exec -it a4h su - a4hadm -c "tp addtobuffer $tfile A4H pf=/usr/sap/trans/bin/TP_DOMAIN_A4H.PFL u168"
 done
